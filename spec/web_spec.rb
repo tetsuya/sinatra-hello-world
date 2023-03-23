3de8a2c6ff25bd306c150ac4c40821c613c9ad41
Tetsuya Kaneko <tetsuya@users.noreply.github.com>
2023-03-23 03:24:06 +0000

    ci: Add rspec

---
+++ spec/web_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path "../spec_helper.rb", __FILE__
+
+describe "web" do
+  context "GET /" do
+    before(:each) do
+      get "/"
+    end
+    it "should return 200 ok" do
+      expect(last_response).to be_ok
+    end
+    it "should return pong" do
+      expect(last_response.body).to eq "Hello, world!"
+    end
+  end
+end
